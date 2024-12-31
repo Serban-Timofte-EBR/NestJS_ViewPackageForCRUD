f89e6dc147500bdbc49e48a0e1cdf9d2f526ad4a
serbantimofte <serban-timofte@outlook.com>
2024-12-31 18:02:49 +0200

    Making the terminal commands to work:

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello, NestJS CRUD CLI!");
+import chalk from 'chalk';
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('nestjs-admin')
+  .description('CLI to generate NestJS admin resources')
+  .version('1.0.0');
+
+program
+  .command('generate <resource>')
+  .description('Generate a new CRUD resource')
+  .action((resource) => {
+    console.log(chalk.green(`Generating resource: ${resource}`));
+  });
+
+program.parse(process.argv);
